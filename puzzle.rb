89de539c1295bc81c9076900510b7a56513b3dee
Jônatas Davi Paganini <jonatasdp@gmail.com>
2016-07-12 19:45:59 -0300

    adding all collection to allow get a random piece

--- puzzle.rb
+++ puzzle.rb
@@ -1,4 +1,3 @@
-
 module GridLock
 
   CROSS = "✚"
@@ -7,21 +6,25 @@ module GridLock
 
   module Pieces
 
-    A = [CROSS,  CIRCLE]
-    B = [CROSS,  SQUARE]
-    C = [SQUARE, CIRCLE]
-    D = [CROSS,  CROSS ]
-    E = [SQUARE, SQUARE]
-    F = [CIRCLE, CIRCLE]
+    ALL = [
 
-    G = [[SQUARE, CIRCLE], SQUARE]
-    H = [[CROSS,  CIRCLE], SQUARE]
-    I = [[CIRCLE, SQUARE], CROSS ]
-    J = [[CROSS,  CROSS ], SQUARE]
-    K = [[CIRCLE, CIRCLE], SQUARE]
-    L = [[SQUARE, CIRCLE], CIRCLE]
-    M = [[CROSS,  SQUARE], CIRCLE]
-    N = [[SQUARE, CROSS ], CIRCLE]
+      A = [CROSS,  CIRCLE],
+      B = [CROSS,  SQUARE],
+      C = [SQUARE, CIRCLE],
+      D = [CROSS,  CROSS ],
+      E = [SQUARE, SQUARE],
+      F = [CIRCLE, CIRCLE],
+
+      G = [[SQUARE, CIRCLE], SQUARE],
+      H = [[CROSS,  CIRCLE], SQUARE],
+      I = [[CIRCLE, SQUARE], CROSS ],
+      J = [[CROSS,  CROSS ], SQUARE],
+      K = [[CIRCLE, CIRCLE], SQUARE],
+      L = [[SQUARE, CIRCLE], CIRCLE],
+      M = [[CROSS,  SQUARE], CIRCLE],
+      N = [[SQUARE, CROSS ], CIRCLE]
+
+    ]
 
   end
 
